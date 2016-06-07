06e72f40f142750227530509ca1722d1f970c07d
jmh <jennifer.m.hays@gmail.com>
2016-06-07 11:27:29 -0400

    Changed output pairs from 1-indexing to 0-indexing. Better comments.

--- run_mRMR.py
+++ run_mRMR.py
@@ -15,8 +15,8 @@ class colors:
     UNDERLINE = '\033[4m'
 
 # Command line arguments
-parser = argparse.ArgumentParser(description='Runs mRMR algorithm. When you provide lists of xtcs, '
-                                             'tprs, and xvgs, ordering matters!')
+parser = argparse.ArgumentParser(description='Runs mRMR algorithm on a list of trajectories. '
+                                             'When you provide lists of xtcs, tprs, and xvgs, ordering matters!')
 parser.add_argument('-f', nargs='*', help='list of xtcs')
 parser.add_argument('-s', nargs='*', help='list of tprs')
 parser.add_argument('-n', help='ndx file for output')
@@ -27,9 +27,10 @@ parser.add_argument('-iters', help='number of times to perform mRMR', type=int)
 parser.add_argument('-binsize', help='bin size in nm', type=float, default=0.2)
 parser.add_argument('-restrict', nargs='*', type=float, help='lower and upper distance limits in nm',
                     default=None)
-parser.add_argument('-w', nargs='*', type=float, help='weights for MID and MIQ in mRMR calculation',
+parser.add_argument('-w', nargs='*', type=float, help='weights for MID (subtraction) and MIQ (division) '
+                                                      'in mRMR calculation, respectively',
                     default=[1.0, 0.0])
-parser.add_argument('-cg', action='store_true', help='Use this option for coarse-grained simulations')
+parser.add_argument('-cg', action='store_true', help='Use this flag for coarse-grained simulations')
 args = parser.parse_args()
 
 xtc_names = args.f
@@ -44,11 +45,16 @@ restrict = args.restrict
 weights = args.w
 aa = not args.cg
 
+# Align all the trajectories and put them in the center of the periodic box so that gmx mindist
+# can calculate distances properly.
+
 print colors.BOLD + '\n\tPreprocessing trajectories' + colors.ENDC
 data, ndx_dict = pre_process(xtcs=xtc_names, tprs=tpr_names, ndx=ndx_name, xvgs=xvg_names,
                              bin_size=bin_size, AA=aa)
 n_samples, n_pairs = data.shape
 
+# Calculate the entropies of each distance pair. If an entropy file already exists, the calculation is
+# skipped and the file is loaded into memory.
 
 print colors.BOLD + '\n\tEntropy of each pair' + colors.ENDC
 
@@ -65,6 +71,9 @@ print colors.BOLD + '\n\tmRMR calculation with ' + str(iters) + ' iterations' + 
 raveled_resis = mRMR(mat=data, weights=weights, restrict=restrict, iters=iters, H=entropy_matrix, bin_size=bin_size)
 print "Highest mRMR pair indices", raveled_resis
 
+
+# Getting the length of the two proteins so that the flattened indices of raveled_resis can be turned
+# into non-flattened indices. In other words, pair 5 -> (ceacam 1, opa 5).
 if aa:
     mol1_len = len(ndx_dict['mol_1_and_name_CA'])
     mol2_len = len(ndx_dict['mol_2_and_name_CA'])
@@ -74,10 +83,11 @@ else:
 
 print "Length of molecule 1:", mol1_len, "\nLength of molecule 2:", mol2_len
 
+# Converts flattened indices to matrix indices.
 resis = np.unravel_index(raveled_resis, (mol1_len, mol2_len))
-resis_indexed_by_1 = np.add(resis, 1)
-print "Converted to residue numbers (Molecule 1, Molecule 2):", resis_indexed_by_1
+print "Converted to residue numbers (Molecule 1, Molecule 2):", resis
+print "Please remember that your residues are now zero-indexed!"
 print "Saving to file", mRMR_name
-np.savetxt(mRMR_name, resis_indexed_by_1, fmt='%g')
+np.savetxt(mRMR_name, resis, fmt='%g')
 
 
