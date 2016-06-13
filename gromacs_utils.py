9be33c13d8378993eb93b9ccb901b2657a42b8cc
jmh <jennifer.m.hays@gmail.com>
2016-06-13 11:57:34 -0400

    1. Added function warn_file_exists. This just prints a warning to the console that you are NOT rewriting files. 2. Better comments. 3. Added capabilities for read_xvg. Now you may bin/get contacts AND keep track of time, whereas before, time could only be read in with raw data.

--- gromacs_utils.py
+++ gromacs_utils.py
@@ -102,7 +102,7 @@ def make_xvg(tpr_filename, xtc_filename, ndx_filename, xvg_filename, length_mol1
     Calculates the distances between all alpha carbons/ backbone atoms of molecule 1 to all alpha carbons/ backbone
     atoms of molecule 2. Distances are calculated at each time step.
     The xvg will therefore have the form:
-    t_0 distance(mol1atom1,mol2atom1) distance(mol1atom2, mol2atom1) ... distance(mol1atomN, mol2atomM)
+    t_0 distance(mol1atom1,mol2atom1) distance(mol1atom1, mol2atom2) ... distance(mol1atomN, mol2atomM)
     t_1 distance(mol1atom1,mol2atom1) ...                                distance(mol1atomN, mol2atomM)
     ...
 
@@ -115,7 +115,7 @@ def make_xvg(tpr_filename, xtc_filename, ndx_filename, xvg_filename, length_mol1
     :param rewrite: boolean to indicate whether you want to overwrite an existing xvg file of the same name
     :return: None. os.system sends the appropriate gmx mindist command to console.
     '''
-    if rewrite or not os.path.exists(xvg_filename):
+    if rewrite or (not os.path.exists(xvg_filename)):
         os.system('echo 2 | gmx mindist -f %s -s %s -n %s -rectmatrix %i -ng 0 -od %s' %
                   (xtc_filename, tpr_filename, ndx_filename, length_mol1, xvg_filename))
     else:
@@ -124,28 +124,30 @@ def make_xvg(tpr_filename, xtc_filename, ndx_filename, xvg_filename, length_mol1
 
 def read_xvg(xvg_filenames, bin_size=0, contact=0, one_traj=True, skip_time=True):
     '''
-    Reads a series of xvg files into a single distance matrix. You may keep each trajectory separate, or concatenate all
-    the trajectories together for use with mRMR.
+    Reads a series of xvg files into a single distance matrix. You may keep track of the time and/or trajectory number
+    with one_traj and skip_time, or read in only the distances. This can perform binning and calculate contacts maps.
+    For use with mRMR, you must bin initially. (Binning while reading the files prevents memory overflow issues).
     :param xvg_filenames: a list of xvg filenames
     :param bin_size: if you wish to bin the distances for use with mRMR, provide a bin size in nm. If bin_size is 0,
     distances will be kept as floating point.
-    :param contact: if you want to get contact maps instead of distances, specify a contact length in nm.
-    :param one_traj: boolean to indicate whether trajectories are concatenated. Must be True
-    for use with mRMR algorithm.
+    :param contact: if you wish to get contact maps instead of distances, specify a contact length in nm.
+    :param one_traj: boolean to indicate whether trajectories are concatenated. Must be True for use with mRMR
+    algorithm.
     :param skip_time: whether you want to read in the time step as well as the distances. This was added for
-    flexibility, but isn't useful for mRMR. Need to set True.
+    flexibility, but isn't useful for mRMR (set True).
     :return: array of distances. data[i,j] = pair j distance value at frame number i if you are not keeping track of
     time or the trajectories. If you are keeping track of the trajectory number, data[i,0] = traj_num. If you are
-    keeping track of the time, data[i,1]=time.
+    keeping track of just the time, data[i,0]=time. If you are keeping track of both trajectory number and time,
+    data[i,0] = traj_num and data[i,1] = time.
     '''
 
     data = []
     traj_num = 0
 
     if bin_size != 0:
-        print colors.HEADER + "Distances will be binned with bin size: " + str(bin_size) + colors.ENDC
+        print "Distances will be binned with bin size:", bin_size
     if contact != 0:
-        print colors.HEADER + "Contact maps will be loaded with contact cutoff: " + str(contact) + " nm" + colors.ENDC
+        print "Contact maps will be loaded with contact cutoff:", contact, "nm"
 
     for xvg_filename in xvg_filenames:
         start_time = time.time()
@@ -163,25 +165,40 @@ def read_xvg(xvg_filenames, bin_size=0, contact=0, one_traj=True, skip_time=True
                     temp_vec = [traj_num]  # The first column of the data should be trajectory number
                 col_num = 0
                 for element in newline.split():
-                    if skip_time:
-                        if col_num > 0:
-                            if bin_size != 0:
-                                dist = int(float(element)/bin_size)
-                            else:
-                                dist = float(element)
+
+                    # BINNING
+                    if bin_size != 0:
+                        if col_num > 0:  # Check to see whether the element is a distance measurement.
+                            dist = int(float(element)/bin_size)  # Bin the distance
                             temp_vec.append(dist)
+                        # If the element is not a distance measurement, check to see if you are keeping track of time
+                        elif not skip_time:
+                            dist = float(element)
+                            temp_vec.append(dist)
+
+                    # CONTACT MAPS
+                    elif contact != 0:
+                        if col_num > 0:  # Check to see whether the element is a distance measurement.
+                            # Determine whether this is a contact or no
+                            if float(element) > contact:
+                                dist = 1
+                            else:
+                                dist = 0
+                            temp_vec.append(dist)
+                        # If the element is not a distance measurement, check to see if you are keeping track of time
+                        elif not skip_time:
+                            dist = float(element)
+                            temp_vec.append(dist)
+
+                    # RAW DATA
                     else:
-                        dist = float(element)
-                        temp_vec.append(dist)
-
-                    if (contact > 0) and (col_num > 0):
-                        if dist < contact:
-                            dist = 1
-                        else:
-                            dist = 0
-                        temp_vec.append(dist)
+                        if col_num > 0:
+                            dist = float(element)
+                            temp_vec.append(dist)
+                        elif not skip_time:
+                            dist = float(element)
+                            temp_vec.append(dist)
                     col_num += 1
-
                 data.append(temp_vec)
         end_time = time.time()
         print "Time elapsed to read trajectory", traj_num, ":", end_time-start_time
@@ -207,20 +224,17 @@ def dump_frame(xtc, tpr, ndx, time, pdb):
 
 def pbc_center(xtcs, tprs, ndx, rewrite=False):
     '''
-    This function is used for pre-processing trajectories that have pbc issues.
-    It does the following:
+    This function is used for pre-processing trajectories that have pbc issues. It does the following:
         1. Make any split atoms whole using gmx trjconv -pbc whole
-        2. Center on a single protein. Ideally, we would center on both proteins.
-           However, there are problems with gmx trjconv -center on multiple
-           chains. See
+        2. Center on a single protein. Ideally, we would center on both proteins. However, there are problems with gmx
+           trjconv -center on multiple chains. See
            https://mailman-1.sys.kth.se/pipermail/gromacs.org_gmx-users/2010-August/053340.html
-           To do: option to center on one or the other protein. Right now we just
-           center on the second protein, Opa.
+           To do: option to center on one or the other protein. Right now we just center on the second protein, Opa.
     :param xtcs: A list of xtcs for pre-processing
-    :param tprs: A list of tprs. Must be in the same order as the xtc list.
+    :param tprs: A list of tprs. MUST BE IN CORRECT ORDER
     :param ndx: A single ndx filename. The ndx file will be written using make_ndx if it
     does not already exist.
-    :param rewrite: Whether to rewrite the ndx file or the intermediate xtc files.
+    :param rewrite: Whether to rewrite the ndx file and the intermediate xtc files.
     :return: None. Writes appropriate gmx commands to console.
     '''
 
@@ -234,13 +248,13 @@ def pbc_center(xtcs, tprs, ndx, rewrite=False):
         cntr_name = stripped[i] + '_cntr.xtc'
 
         if os.path.exists(whole_name) and not rewrite:
-            print "The file", whole_name, "already exists: use rewrite=True to override"
+            warn_file_exists(whole_name)
         else:
             os.system('echo 3 3 | gmx trjconv -f %s -s %s -pbc whole -n %s -o %s' %
                       (orig_xtcs[i], tprs[i], ndx, whole_name))
 
         if os.path.exists(cntr_name) and not rewrite:
-            print "The file", cntr_name, "already exists: use rewrite=True to override"
+            warn_file_exists(cntr_name)
         else:
             os.system('echo 1 3 | gmx trjconv -f %s -s %s -pbc mol -ur compact -center -n %s -o %s' %
                       (whole_name, tprs[i], ndx, cntr_name))
@@ -248,15 +262,14 @@ def pbc_center(xtcs, tprs, ndx, rewrite=False):
 
 def write_pdbs(xtcs, tprs, ndx, pdbs, dt=100000, rewrite=False):
     '''
-    Uses gmx trjconv to write out pdbs at intervals of dt. The pdbs are written with connect
-    records. This is really for visualization to make sure centered trajectories are not
-    moving throught the periodic box.
+    Uses gmx trjconv to write out pdbs at intervals of dt. The pdbs are written with connect records. This is really for
+    visualization to make sure centered trajectories are not moving through the periodic box. It is not called in mRMR
+    algorithm, so if you want to check you need to write your own script.
     :param xtcs: A list of xtcs.
     :param tprs: A list of tprs. MUST BE IN CORRECT ORDER
-    :param ndx: An index file of the form described in the method make_ndx
+    :param ndx: An index file of the form described in the method make_ndx.
     :param pdbs: A list of output pdbs. Must be the same length as your xtc list.
-    :param dt: Intervals at which you wish to write out pdbs. See GROMACS documentation of
-    trjconv.
+    :param dt: Intervals at which you wish to write out pdbs. See GROMACS documentation of trjconv.
     :param rewrite: Whether to overwrite the pdbs.
     :return: None. Writes appropriate gmx commands to console.
     '''
@@ -277,18 +290,15 @@ def pre_process(xtcs, tprs, ndx, xvgs, AA=True, rewrite=False):
         3. Takes care of pbc problems by clustering and centering with pbc_center.
         4. Makes a bunch of xvg files using a list of xtcs and tprs.
     :param xtcs: list of xtcs
-    :param tprs: list of tprs
-    :param ndx: single index file name. If the index file does not already exist, one will
-    be created using make_ndx
+    :param tprs: list of tprs. MUST BE IN CORRECT ORDER.
+    :param ndx: single index file name. If the index file does not already exist, one will be created using make_ndx
     :param xvgs: a list of xvg file names. If any one of the xvgs does not already exist, it
     will be created using make_xvg.
     :param AA: indicates whether the trajectories are all-atom or coarse-grained.
-    :param rewrite: boolean to indicate whether you want to overwrite an existing index file of the
-    same name
+    :param rewrite: boolean to indicate whether you want to overwrite an existing index file of the same name.
     :return None. Writes appropriate gmx commands to console.
     '''
 
-    cntr_xtcs = []
     n_files = 0
 
     make_ndx(tprs[0], ndx, rewrite=rewrite, AA=AA)
@@ -300,12 +310,10 @@ def pre_process(xtcs, tprs, ndx, xvgs, AA=True, rewrite=False):
 
     pbc_center(xtcs, tprs, ndx)
 
-    for xtc in xtcs:
-        if 'cntr' in xtc:
-            cntr_xtcs.append(xtc)
-            n_files += 1
+    cntrd_xtcs = [strip + '_cntr.xtc' for strip in strip_filenames(xtcs)]
+    n_files = len(cntrd_xtcs)
 
     for i in range(n_files):
 
-        make_xvg(tpr_filename=tprs[i], xtc_filename=cntr_xtcs[i], ndx_filename=ndx,
+        make_xvg(tpr_filename=tprs[i], xtc_filename=cntrd_xtcs[i], ndx_filename=ndx,
                  xvg_filename=xvgs[i], length_mol1=size_mol1, rewrite=rewrite)
